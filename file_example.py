f950ffd77398e68208985cb01ff8911da50d4614
amedeocroci <amedeo.croci@gmail.com>
2025-02-05 17:02:44 +0100

    first commit

--- file_example.py
+++ file_example.py
@@ -1,3 +1,8 @@
 fp=open("text.txt", "r") #r is by default so not really needed
 print(fp.read()) #prints the entire content of the file
 fp.close() #good practice to close the file
+
+#same exact thing with context manager
+with open("text.txt", "r") as fp:
+    print(fp.read())
+
